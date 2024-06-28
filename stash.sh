118974d77345f73f8848ce827e0336e9d4eb0721
Devin Leamy <devinleamy@gmail.com>
2024-06-27 23:41:09 -0700

    Update dotfiles

--- stash.sh
+++ stash.sh
@@ -10,7 +10,6 @@ HAMMERSPOON="$HOME/.hammerspoon"
 SKETCHYBAR="$HOME/.config/sketchybar"
 FISH="$HOME/.config/fish"
 NVIM="$HOME/.config/nvim"
-ZSH="$HOME/.zshrc"
 TMUX="$HOME/.tmux.conf"
 ALACRITTY="$HOME/.config/alacritty"
 
@@ -18,7 +17,6 @@ HAMMERSPOON_SYM="$REPO/.hammerspoon"
 SKETCHYBAR_SYM="$REPO/sketchybar"
 NVIM_SYM="$REPO/nvim"
 FISH_SYM="$REPO/fish"
-ZSH_SYM="$REPO/.zshrc"
 TMUX_SYM="$REPO/.tmux.conf"
 ALACRITTY_SYM="$REPO/alacritty"
 
@@ -27,7 +25,6 @@ unlink $HAMMERSPOON_SYM
 unlink $SKETCHYBAR_SYM
 unlink $NVIM_SYM
 unlink $FISH_SYM
-unlink $ZSH_SYM
 unlink $TMUX_SYM
 unlink $ALACRITTY_SYM
 
@@ -36,7 +33,6 @@ cp -r $HAMMERSPOON $HAMMERSPOON_SYM
 cp -r $SKETCHYBAR $SKETCHYBAR_SYM
 cp -r $NVIM $NVIM_SYM
 cp -r $FISH $FISH_SYM
-cp -r $ZSH $ZSH_SYM
 cp -r $TMUX $TMUX_SYM
 cp -r $ALACRITTY $ALACRITTY_SYM
 
@@ -50,7 +46,6 @@ rm -rf $HAMMERSPOON_SYM
 rm -rf $SKETCHYBAR_SYM
 rm -rf $NVIM_SYM
 rm -rf $FISH_SYM
-rm -rf $ZSH_SYM
 rm -rf $TMUX_SYM
 rm -rf $ALACRITTY_SYM
 
@@ -59,6 +54,5 @@ ln -s $HAMMERSPOON $HAMMERSPOON_SYM
 ln -s $SKETCHYBAR $SKETCHYBAR_SYM
 ln -s $NVIM $NVIM_SYM
 ln -s $FISH $FISH_SYM
-ln -s $ZSH $ZSH_SYM
 ln -s $TMUX $TMUX_SYM
 ln -s $ALACRITTY $ALACRITTY_SYM
