d88e7a82bbd621d480edc4245319757faf3a7050
Devin Leamy <devinleamy@gmail.com>
2024-07-14 19:56:49 -0700

    Update dotfiles

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,5 @@
 -- Defaults: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
 vim.opt.conceallevel = 0 -- Show explicit Markdown file contents.
+
+vim.opt.relativenumber = false -- Show absolute line numbers.
