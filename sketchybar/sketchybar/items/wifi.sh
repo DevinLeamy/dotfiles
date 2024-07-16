89cce8a144657c2aa68eff63ffee71beb7c29f10
Devin Leamy <devinleamy@gmail.com>
2024-07-15 20:55:02 -0700

    Update dotfiles

--- sketchybar/sketchybar/items/wifi.sh
+++
@@ -1,7 +0,0 @@
-sketchybar --add item wifi right                              \
-           --set wifi    script="$PLUGIN_DIR/wifi.sh"          \
-                        icon=                               \
-                        label.padding_right=0                \
-                        label.padding_left=0                 \
-           --subscribe wifi wifi_change                        \
-            
