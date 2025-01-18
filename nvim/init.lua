abe1c7f39a1923c66904cdc9e681a8867a66d33e
Devin Leamy <devinleamy@gmail.com>
2025-01-18 10:47:26 -0500

    Update dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,7 +6,7 @@ vim.opt.swapfile = false
 
 -- Autosave files.
 vim.api.nvim_create_autocmd("BufModifiedSet", {
-    pattern = "*.rs",
+    pattern = { "*.rs", "*.c", "*.h", "*.lua", "*.ts", "*.js" },
     command = "silent! write",
 })
 
@@ -15,6 +15,20 @@ require("lspconfig").svelte.setup({})
 
 -- Enable 'typescript-language-server'.
 require("lspconfig").tsserver.setup({})
+require("lspconfig").eslint.setup({})
+
+require("lspconfig").clangd.setup({})
+
+require("lspconfig")["lua_ls"].setup({
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+                path = "/Users/Devin/.local/lua/lua",
+            },
+        },
+    },
+})
 
 -- Toggle comments with Ctrl+/.
 require("Comment").setup()
